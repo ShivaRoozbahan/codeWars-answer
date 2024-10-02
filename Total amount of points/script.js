ab39d917e4a55f0b2bda82d365b157cb2979226d
Shiva <shivaroozbahani@gmail.com>
2024-10-02 17:41:25 +0330

    points

---
+++ Total amount of points/script.js
@@ -0,0 +1,19 @@
+// let result = 0;
+// function points(games) {
+//   x = games.map((item) => item.charAt(0));
+//   y = games.map((item) => item.charAt(2));
+//   if (x > y) {
+//     result = +3;
+//   }
+//   if (x < y) {
+//     result = +0;
+//   } else {
+//     result = +1;
+//   }
+//   return result;
+// }
+
+// console.log(points(["5:4", "0:2", "3:0", "4:3"]));
+
+// نتونستم حلش کنممم 😭😭😭😭😭
+//  ولی حلش میکنم😶‍🌫️
